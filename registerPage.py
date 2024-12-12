d8b32bfe4ce2d9f0bb25e35f91c47fc6743fe32c
Jake14turner <110646968+Jake14turner@users.noreply.github.com>
2024-12-11 18:35:41 -0600

    Update registerPage.py

--- registerPage.py
+++ registerPage.py
@@ -8,9 +8,5 @@ def registerPageView():
     #This is the User components responsibilty: we need to register as a user so 
     register()
 
-    login = st.button("Log in")
-    if login:
-        st.switch_page("loginPage.py")
-
 registerPageView()
 
